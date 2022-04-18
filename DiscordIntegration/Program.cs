8861b1cfadb4dcdd081b7bbed1b9afb0acd85bed
dodo01 <cos_leb@yahoo.com>
2022-04-19 00:31:55 +0300

    start working on project

--- DiscordIntegration/Program.cs
+++ DiscordIntegration/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DiscordIntegration
+namespace DiscordIntegration.WebAPI
 {
     public class Program
     {
